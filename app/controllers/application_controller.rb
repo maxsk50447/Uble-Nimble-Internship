10d8f3ac3215518ee8d70e7623818236c8febc0f
Patipol Wangjaitham <patipol.wa@ku.th>
2018-03-08 14:25:18 +0700

    Move configure_permitted_parameters in to validatable.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,28 +4,9 @@ class ApplicationController < ActionController::Base
   # after_action :verify_policy_scoped, only: :index
   protect_from_forgery
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError do
     flash[:alert] = "Access denied."
     redirect_to (request.referrer || root_path)
   end
 
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:first_name,
-                                              :last_name,
-                                              :citizen_id,
-                                              :student_id,
-                                              :instructor_id,
-                                              :department])
-    devise_parameter_sanitizer.permit(:account_update,
-                                      keys: [:first_name,
-                                      :last_name,
-                                      :citizen_id,
-                                      :student_id,
-                                      :instructor_id,
-                                      :department])
-  end
 end
