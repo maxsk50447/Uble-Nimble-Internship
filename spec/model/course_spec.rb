4190c2b9a8da175802d890f50e5f766a2e99e788
Patipol Wangjaitham <patipol.wa@ku.th>
2018-03-29 23:13:22 +0700

    Delete model folder

--- spec/model/course_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Course, type: :model do
-
-  describe 'associations' do
-    it { should have_and_belong_to_many(:users) }
-    it { should belong_to(:instructor) }    
-  end
-end
