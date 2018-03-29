71267cfb551f2f7c4abf46622e2232fdca26d5ce
Patipol Wangjaitham <patipol.wa@ku.th>
2018-03-29 23:14:40 +0700

    Change method that create user

--- spec/policies/course_policy_spec.rb
+++ spec/policies/course_policy_spec.rb
@@ -5,7 +5,7 @@ describe CoursePolicy do
   context 'being a user' do
     subject { described_class.new(user, course) }
     let(:course) { nil }
-    let(:user) { User.create }
+    let(:user) { FactoryBot.create(:user) }
     it {is_expected.to forbid_action(:new)}
     it {is_expected.to forbid_action(:create)}
     it {is_expected.to permit_action(:show)}
@@ -15,7 +15,7 @@ describe CoursePolicy do
   context 'being a student' do
     subject { described_class.new(student, course) }
     let(:course) { nil }
-    let(:student) { Student.create }
+    let(:student) { FactoryBot.create(:student) }
     it {is_expected.to forbid_action(:new)}
     it {is_expected.to forbid_action(:create)}
     it {is_expected.to permit_action(:show)}
@@ -25,7 +25,7 @@ describe CoursePolicy do
   context 'being an instructor' do
     subject { described_class.new(instructor, course) }
     let(:course) { nil }
-    let(:instructor) { Instructor.create }
+    let(:instructor) { FactoryBot.create(:instructor) }
     it {is_expected.to permit_action(:new)}
     it {is_expected.to permit_action(:create)}
     it {is_expected.to permit_action(:show)}
