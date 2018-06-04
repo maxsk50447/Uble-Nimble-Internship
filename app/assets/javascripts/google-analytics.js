469873c9c937f5dc2a410970a1a82f6e48388627
Patipol Wangjaitham <patipol.wa@ku.th>
2018-06-04 10:43:32 +0700

    Change something

--- app/assets/javascripts/google-analytics.js
+++ app/assets/javascripts/google-analytics.js
@@ -12,21 +12,16 @@
 
   Uble.App.GoogleAnalytics = {
 
-    initGooglyAnalytics: function () {
+    initGooglyAnalytics: function () {      
       (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-          (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-          m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
       })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-      ga('create', 'UA-120231444-1', 'auto');
-      
-      document.addEventListener('turbolinks:load', function (event) {
-        if (typeof ga === 'function') {
-          ga('set', 'location', event.data.url);
-          return ga('send', 'pageview');
-        }
-      });
+
+      ga('create', 'UA-XXXXX-Y', 'auto');
+      ga('send', 'pageview');
     },
 
     Init: function () {
